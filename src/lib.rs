ca26dde18104aa34c5e31ceba2c22dba098f3f48
Alol <aloisdsc@protonmail.com>
2026-06-30 11:03:58 +0200

    refactor: use more ideomatic syntax and functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 use sha2::{Digest, Sha256};
 
-fn be24(n: usize) -> [u8; 3] {
+fn be24(n: u64) -> [u8; 3] {
     n.to_be_bytes()[5..].try_into().unwrap()
 }
 
-fn be40(n: usize) -> [u8; 5] {
+fn be40(n: u64) -> [u8; 5] {
     n.to_be_bytes()[3..].try_into().unwrap()
 }
 
-fn be56(n: usize) -> [u8; 7] {
+fn be56(n: u64) -> [u8; 7] {
     n.to_be_bytes()[1..].try_into().unwrap()
 }
 
-fn be64(n: usize) -> [u8; 8] {
+fn be64(n: u64) -> [u8; 8] {
     n.to_be_bytes()
 }
 
 pub fn chunk_hash(s: &[u8]) -> [u8; 32] {
     if s.len() < 1024 {
-        let mut hasher = Sha256::new();
-        hasher.update(s);
-        hasher.update(b"/");
-        hasher.finalize().into()
+        Sha256::new()
+            .chain_update(s)
+            .chain_update(b"/")
+            .finalize()
+            .into()
     } else {
         sha256x16(s)
     }
@@ -36,21 +37,23 @@ pub fn sha256x16(s: &[u8]) -> [u8; 32] {
         }
     }
 
-    let mut h = Sha256::new();
+    let mut hasher = Sha256::new();
 
     for lane in lanes {
-        h.update(lane.finalize());
+        hasher.update(lane.finalize());
     }
 
-    h.update(be64(s.len()));
-    h.update(b"/J16");
-    h.finalize().into()
+    hasher
+        .chain_update(be64(s.len() as u64))
+        .chain_update(b"/J16")
+        .finalize()
+        .into()
 }
 
 pub fn chunk_list(s: &[u8]) -> Vec<u8> {
     let mut out = Vec::<u8>::new();
 
-    for block in s.chunks(usize::pow(2, 21)) {
+    for block in s.chunks(1 << 21) {
         out.extend(chunk_hash(block));
     }
 
@@ -63,22 +66,22 @@ pub fn chunk_list(s: &[u8]) -> Vec<u8> {
 pub fn psha2(s: &[u8]) -> Vec<u8> {
     let mut out = Vec::<u8>::new();
 
-    if s.len() == 0 {
+    if s.is_empty() {
         out.push(0);
-    } else if s.len() <= usize::pow(2, 21) {
+    } else if s.len() <= 1 << 21 {
         out.push(1);
-        out.extend(be24(s.len()));
+        out.extend(be24(s.len() as u64));
         out.extend(chunk_hash(s))
-    } else if s.len() <= usize::pow(2, 37) - usize::pow(2, 21) {
+    } else if s.len() <= (1 << 37) - (1 << 21) {
         out.push(2);
-        out.extend(be40(s.len()));
+        out.extend(be40(s.len() as u64));
         out.extend(chunk_hash(&chunk_list(s)));
-    } else if s.len() <= usize::pow(2, 52) {
+    } else if s.len() <= 1 << 52 {
         out.push(3);
-        out.extend(be56(s.len()));
+        out.extend(be56(s.len() as u64));
         out.extend(chunk_hash(&chunk_list(&chunk_list(s))));
     } else {
-        todo!()
+        unimplemented!("Hashing data larger than 2**52B is undefined in the spec.")
     }
 
     out
